587e23e80944e7b2f34820fdc3cd697363178966
Irina-Kovtun <irinakovtun.us@gmail.com>
2020-10-26 00:38:02 +0300

    EqualsIgnoreCase is added

--- src/main/java/ru/netology/repository/FlightRepository.java
+++ src/main/java/ru/netology/repository/FlightRepository.java
@@ -23,7 +23,7 @@ public class FlightRepository {
     public Flight[] findByAirports(String from, String to) {
         Flight[] result = new Flight[0];
         for (Flight item : items) {
-            if ((item.getDeparture() == from) && (item.getArrival() == to)) {
+            if ((item.getDeparture().equalsIgnoreCase(from)) && (item.getArrival().equalsIgnoreCase(to))) {
                 Flight[] tmp = new Flight[result.length + 1];
                 System.arraycopy(result, 0, tmp, 0, result.length);
                 tmp[tmp.length - 1] = item;
